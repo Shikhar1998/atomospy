f29d5ba98a2530cd6cba1b6101e93d1660e25d7a
Shikhar Makhija <shikharmakhija2@gmail.com>
2018-01-29 00:02:54 +0530

    A trivial add on

---
+++ dimension.py
@@ -0,0 +1,20 @@
+"""
+Implementation of Dimension
+A dimension is anything that has a particular length. Ranging
+from a point in the real negative infinity to a point positive
+infinity.
+"""
+
+def _zero_dimension(args):
+    """
+    The smallest unit of dimension - a point in the space time
+    continum.
+    """
+    return 0
+
+def _dimension():
+    """
+    A continuum of infinite zero dimensions ranging from negative
+    to positive infinity.
+    """
+    return 0
--- strings.py
+++ strings.py
@@ -1,6 +1,17 @@
 """
 Implementation of a basic string in string theory
 """
-
+from atomospy.dimensions import Dimension
 def _string():
+    """
+    The strings are a one dimensional objects.
+    A string can be openended or closed loop. Moreover, their
+    exists an orientation in the string. A 'arrow' that dist-
+    -inguishes it with a another. A string can also be orien-
+    -tationless.
+    Reference: https://en.wikipedia.org/wiki/String_(physics)
+    """
     return 0
+
+def __string_length():
+    return pow(10, -35)
